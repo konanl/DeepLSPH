8f0ff8845b883c43236dc310434518872ec8bfb4
yan liangliang <2625058389@qq.com>
2022-03-19 20:05:51 +0800

    2022-3-19 first update

---
+++ yoking/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+    @author: yan liangliang
+    @license: (C) Copyright forever
+    @contact: yokingyan@gmail.com
+    @ide: pycharm
+    @file: main.py
+    @time: 2022/3/18 21:06
+    @desc: this is a test python file
+    
+"""
+import torch
+
---
+++ yoking/trainNet.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+    @author: yan liangliang
+    @license: (C) Copyright forever
+    @contact: yokingyan@gmail.com
+    @ide: pycharm
+    @file: trainNet.py
+    @time: 2022/3/19 16:44
+    @desc: this python file is to training my network which is based on the API of d2l
+           if you don't have the d2l package, please try - pip install d2l(maybe you need a VPN)
+    
+"""
+import torch
+import d2l
+
+
+
